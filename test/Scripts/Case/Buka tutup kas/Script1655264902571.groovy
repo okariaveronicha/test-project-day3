d088fc95d522a8eaa9680ef60dc095bcd95b45be
unknown <okaria.konsultan@work.bri.co.id>
2022-06-22 11:42:45 +0700

    baru

--- test/Scripts/Case/Buka tutup kas/Script1655264902571.groovy
+++ test/Scripts/Case/Buka tutup kas/Script1655264902571.groovy
@@ -21,18 +21,18 @@ import java.util.Random as Random
 Mobile.startExistingApplication('id.co.bri.stroberikasir')
 
 //Mobile.callTestCase(findTestCase('Case/Login retailer'), [:], FailureHandling.STOP_ON_FAILURE)
-//Mobile.setText(findTestObject('Object Repository/Login/android.widget.EditText - Nomor Ponsel'), '0812345678906', 0)
-//
-//Mobile.setEncryptedText(findTestObject('Object Repository/Login/android.widget.EditText - Kata Sandi'), 'iFGeFYmXIrUhQZHvW7P22w==', 0)
-//
-//Mobile.tap(findTestObject('Object Repository/Login/android.widget.Button - Masuk'), 0)
-//
-//Mobile.tap(findTestObject('Object Repository/Buka tutup kas/android.widget.Button - Tutup Kas'), 0)
+Mobile.setText(findTestObject('Object Repository/Login/android.widget.EditText - Nomor Ponsel'), '0812345678903', 0)
 
-Random rnd = new Random()
-randomNumber =  (1000000 + rnd.nextInt(9999999))
+Mobile.setEncryptedText(findTestObject('Object Repository/Login/android.widget.EditText - Kata Sandi'), 'iFGeFYmXIrUhQZHvW7P22w==', 0)
 
-Mobile.setText(findTestObject('Object Repository/Buka tutup kas/android.widget.EditText - Masukkan saldo kas tunai disini'), String.valueOf(randomNumber), 0)
+Mobile.tap(findTestObject('Object Repository/Login/android.widget.Button - Masuk'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Buka tutup kas/android.widget.Button - Tutup Kas'), 0)
+
+//Random rnd = new Random()
+//randomNumber =  (1000000 + rnd.nextInt(9999999))
+
+Mobile.setText(findTestObject('Object Repository/Buka tutup kas/android.widget.EditText - Masukkan saldo kas tunai disini'), '2365000', 0)
 
 Mobile.tap(findTestObject('Object Repository/Buka tutup kas/android.widget.Button - Buka Kas'), 0)
 
---
+++ test/Test Suites/New Test Suite.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>234f446f-83ca-4642-85c8-6335d967f20c</testSuiteGuid>
+   <testCaseLink>
+      <guid>5a98fffb-bf26-4ad6-a5e7-4695255c51a7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test case/Fitur bulk upload retailer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>38248722-5209-45a6-aa65-30e8841490d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test case/Fitur daftar pesanan retailer dalam proses</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c814f201-b709-4b09-a1bb-060bf4d82cce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test case/Fitur generate laporan transaksi</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1fbcc2df-6aec-4e36-9d91-63bd330fb30e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test case/Fitur hapus retailer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>113b5704-a65b-4c96-8028-a53bed3885e3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Case/Inbox order</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>db3ec1cf-bde7-4b41-9369-5f977bc60a1c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test case/Fitur inbox pesanan</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1a0adb24-27cb-44c4-b8b8-4d5542dc9cda</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Case/Kelola stok produk</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>15c42d4c-fabf-44b0-a6f7-a78d2e4c9a0c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Case/Fitur bayar pesanan</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1e24945c-fcd3-4e09-aec5-e11e60939617</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test case/Fitur penyesuaian show product ke retailer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cd8aa217-83f6-4bfa-9a12-0f1c702471b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test case/Fitur tambah retailer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dcb2f1b0-ae7f-4087-9a54-cbf930bc396f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test case/Fitur daftar supplier</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>003ebfeb-519d-4abe-9a95-5f5ee86a5e61</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test case/Fitur retailer terima pesanan</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
